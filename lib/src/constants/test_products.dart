137aa9640bbca0d6e17063705cfd21f8e07f8e4e
michael barilla <michaelcbarilla@gmail.com>
2025-05-29 11:43:26 -0400

    add test tasks to begin widget development

---
+++ lib/src/constants/test_products.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_task_manager/src/features/tasks/domain/task.dart';
+
+/// Test Tasks to be used until a data source is implemented
+const kTestTasks = [
+  Task(pid: '1', name: 'Video Game'),
+  Task(pid: '2', name: 'Firefox'),
+  Task(pid: '3', name: 'Chrome'),
+  Task(pid: '4', name: 'Email'),
+  Task(pid: '5', name: 'Slack'),
+  Task(pid: '6', name: 'Teams'),
+  Task(pid: '7', name: 'Application'),
+  Task(pid: '8', name: 'Code'),
+];
